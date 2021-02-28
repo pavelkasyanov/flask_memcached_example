aee37d707a93735b5c7c475747c2cde8f306047a
pavelkasyanov <paulkassianov@gmail.com>
2021-02-28 20:47:22 +0300

    remove logging

--- src/application.py
+++ src/application.py
@@ -13,9 +13,6 @@ def create_app() -> Flask:
     container.config.host.from_env('CACHE_HOST')
     container.config.port.from_env('CACHE_PORT')
 
-    logging.basicConfig(level=logging.DEBUG)
-    app.logger.info(f'config: h:{container.config.host()}, p:{container.config.port()}')
-
     container.wire(modules=[fibonacci])
 
     app.container = container
